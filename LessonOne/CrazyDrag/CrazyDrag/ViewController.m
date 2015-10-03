c159dfd1a77530d35af2940f2b2e623d7a054d8a
vaithwee <apple@appledemac-mini.local>
2015-10-03 17:58:49 +0800

    完成第六篇的学习。 增加了UISlider的属性，获取其值，并用NSLog打印出来。

--- LessonOne/CrazyDrag/CrazyDrag/ViewController.m
+++ LessonOne/CrazyDrag/CrazyDrag/ViewController.m
@@ -8,20 +8,51 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    //全局变量，当前滑动条的值
+    int currentValue;
+    //随机数，1-100以内的整数
+    int targetValue;
+}
+//关联滑动条的滑动事件
+-(IBAction)sliderMoved:(id)sender;
+
+//outlet，SB对象关联的属性变量
+@property (strong, nonatomic) IBOutlet UISlider *slider;
+
+-(IBAction)showAlert:(id)sender;
 
 @end
 
+//----------上面是Interface，下面是Implementation-------------
+
 @implementation ViewController
+//synthesize会自动在ViewController中添加代码，以使用Property；
+@synthesize slider;
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //初始化当前值
+    //currentValue=50;
+    currentValue=self.slider.value;
+    NSLog(@"当前值=%d",currentValue);
+    targetValue=1+(arc4random()%100);
 }
 
+- (IBAction)sliderMoved:(UISlider*)sender {
+//    UISlider *slider=(UISlider*)sender;
+    currentValue = (int)lroundf(sender.value);
+    NSLog(@"滑动条的当前数值：%d",currentValue);
+}
+
+/*
+ 弹出对话框
+ */
 - (IBAction)showAlert:(id)sender {
-    UIAlertController* alert=[UIAlertController alertControllerWithTitle:@"您好，苍老师" message:@"听说您的新帖转发了499次" preferredStyle:UIAlertControllerStyleAlert];
-    UIAlertAction* defaultAction=[UIAlertAction actionWithTitle:@"我来帮忙转一次" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+    UIAlertController* alert=[UIAlertController alertControllerWithTitle:@"灰色竞彩机器" message:@"究竟是大还是小呢？" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction* defaultAction=[UIAlertAction actionWithTitle:@"我要下注" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
             }];
     [alert addAction:defaultAction];
     
